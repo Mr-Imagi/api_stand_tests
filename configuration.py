a90363068591ae170c818450cef5bfe76ad78833
Danko Abarca <you@example.com>
2024-04-21 21:08:31 -0400

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,19 @@
+#Acceso a la API de Urban
+URL_SERVICE = "https://cnt-389e30b6-66c9-4105-ae75-8f2a49c82ea0.containerhub.tripleten-services.com"
+
+#La ruta donde se encuentra el documento
+DOC_PATH = "/docs/"
+
+#Ruta para conseguir los logs
+LOG_MAIN_PATH = "/api/logs/main/"
+
+#Ruta para recuperar informacion de la tabla de datos
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+
+#Solicitudes POST
+#Crear Usuario
+CREATE_USER_PATH = "/api/v1/users/"
+
+#Buscar kits
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
